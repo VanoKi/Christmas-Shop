58608d5785ea4f4c074f371eac8a6d2a60323710
VanoKi <kirsvano@gmail.com>
2024-11-27 17:06:13 +0300

    feat: add a date in to the console.log

--- christmas-shop/script.js
+++ christmas-shop/script.js
@@ -1,1 +1,7 @@
-console.log('Hi')
+let now = new Date()
+console.log(now)
+let date = new Date('2025-01-01')
+console.log(date)
+let newYear = new Date(2025, 0, 1)
+console.log(newYear)
+console.log(newYear - now)
