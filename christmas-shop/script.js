04a4064f851309de1e850d9f87af8d26b314a043
VanoKi <kirsvano@gmail.com>
2024-11-29 15:45:49 +0300

    fix: img in random cards

--- christmas-shop/script.js
+++ christmas-shop/script.js
@@ -15,10 +15,10 @@ async function displayGifts() {
     let gifts = await getGifts()
     gifts = gifts.sort(() => .5  - Math.random()).slice(0, 4)
     for (let i = 0; i < gifts.length; i++) {
-        console.log(gifts[i].name)
+        // console.log(gifts[i].category.split(' ')[1].toLowerCase())
         block += `
             <div class="gift__card">
-          <img src="assets/img/img-compressed/gift-for-work.png" alt="gift-for-work">
+          <img src="assets/img/img-compressed/gift-for-${gifts[i].category.split(' ')[1].toLowerCase()}.png" alt="gift-for-work">
           <div class="gift__card-titles">
             <p class="gift__card-subtitle card__for-work">${gifts[i].category}</p>
             <h3 class="gift__card-title">${gifts[i].name}</h3>
