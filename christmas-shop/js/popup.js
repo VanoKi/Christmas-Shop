d3fe9b31c0d9a2d7cc53c71929d54b21f47da160
VanoKi <kirsvano@gmail.com>
2024-11-29 20:48:57 +0300

    fix: popup on both pages

--- christmas-shop/js/popup.js
+++ christmas-shop/js/popup.js
@@ -1,10 +1,16 @@
 import {getGifts} from "./displayGifts.js";
+const gifts = await getGifts()
 
 export function clickOnCard() {
     document.querySelectorAll('.gift__card').forEach( (card) => {
         card.addEventListener('click', (event) => {
             let clickedCard = card.querySelector('h3').textContent
             console.log(clickedCard)
+            for (let i = 0; i < gifts.length; i++) {
+                if (clickedCard === gifts[i].name) {
+                    console.log(gifts[i])
+                }
+            }
         })
     })
 }
