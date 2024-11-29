cd8f6fb42b5755868ad600eb36f97964c9662145
VanoKi <kirsvano@gmail.com>
2024-11-29 16:11:07 +0300

    fix: moved displayGifts to special module

---
+++ christmas-shop/js/displayGifts.js
@@ -0,0 +1,24 @@
+async function getGifts() {
+    let response = await fetch('gifts.json')
+    let data = await response.json()
+    return data
+}
+
+export async function displayGifts() {
+    let block = ``
+    let gifts = await getGifts()
+    gifts = gifts.sort(() => .5  - Math.random()).slice(0, 4)
+    for (let i = 0; i < gifts.length; i++) {
+        // console.log(gifts[i].category.split(' ')[1].toLowerCase())
+        block += `
+            <div class="gift__card">
+          <img src="assets/img/img-compressed/gift-for-${gifts[i].category.split(' ')[1].toLowerCase()}.png" alt="gift-for-work">
+          <div class="gift__card-titles">
+            <p class="gift__card-subtitle card__for-work">${gifts[i].category}</p>
+            <h3 class="gift__card-title">${gifts[i].name}</h3>
+          </div>
+        </div>
+        `
+    }
+    document.querySelector('.gifts__wrap').innerHTML += block
+}
