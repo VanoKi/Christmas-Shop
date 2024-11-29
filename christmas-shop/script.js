46318e60079d2bd24728e82cb85dc850cc1525e1
VanoKi <kirsvano@gmail.com>
2024-11-29 14:24:01 +0300

    feat: add data from gifts.js to main script

--- christmas-shop/script.js
+++ christmas-shop/script.js
@@ -2,3 +2,21 @@ import {countdown} from "./js/timer.js";
 
 setInterval( countdown, 1000)
 
+async function getGifts() {
+    let response = await fetch('gifts.json')
+    let data = await response.json()
+    return data
+}
+
+// console.log(await getGifts())
+
+async function displayGifts() {
+    let gifts = await getGifts()
+    console.log(gifts)
+}
+
+displayGifts()
+
+// getGifts().then(gifts => {
+//     console.log(gifts)
+// })
