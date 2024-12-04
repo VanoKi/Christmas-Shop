b4778aaab5f9e0e97794e36808189a00492a69af
VanoKi <kirsvano@gmail.com>
2024-12-04 23:32:45 +0300

    feat: create a slider function

--- christmas-shop/script.js
+++ christmas-shop/script.js
@@ -3,6 +3,7 @@ import {displayGifts} from "./js/displayGifts.js";
 import {getGifts} from "./js/displayGifts.js";
 import {clickOnCard} from "./js/popup.js";
 import {showBurger} from "./js/burger.js";
+import {slider} from "./js/slider.js";
 
 setInterval( countdown, 1000)
 const gifts = await getGifts()
@@ -11,4 +12,4 @@ const destination = document.querySelector('.gifts__wrap')
 showBurger()
 displayGifts(4, destination, gifts)
 clickOnCard()
-
+slider()
