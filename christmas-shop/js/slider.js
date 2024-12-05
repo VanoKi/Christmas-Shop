e438fba20b632b691b8e530a190d8934a6dbf9e1
VanoKi <kirsvano@gmail.com>
2024-12-05 19:23:45 +0300

    feat: work slider

--- christmas-shop/js/slider.js
+++ christmas-shop/js/slider.js
@@ -1,29 +1,56 @@
 export function slider() {
     const row = document.querySelector('.row')
     const slider = document.querySelector('.slider')
-
-    const btnRight = document.querySelector('#right')
     const btnLeft = document.querySelector('#left')
-
-    let count = 3
-    // console.log(row.offsetWidth)
-    let width = window.innerWidth > 768 ? 3 : 6
-    // let width = 135
-    console.log(width)
+    const btnRight = document.querySelector('#right')
+    let width = 0
+    let step = 0
     let position = 0
 
-    btnRight.addEventListener('click', () => {
-        position -= width * count
-        position = Math.max(position, -width * (row.childElementCount - count))
-        row.style.transform = `translateX(${position + 'px'})`
-    })
+    function calc() {
+        width = row.scrollWidth
+        const visibleWidth = slider.offsetWidth
+        const size = window.innerWidth > 768 ? 3 : 6
+        step = (width - visibleWidth) / size
+        position = 0
+        row.style.transform = `translateX(${position}px)`
+        updateBtns()
+    }
 
-    btnLeft.addEventListener('click', () => {
-        position += width * count
-        position = Math.max(position, 0)
-        // row.style.marginLeft = position + 'px'
-    })
+    function updateBtns() {
+        if (position === 0) {
+            btnLeft.classList.add('disabled')
+        } else {
+            btnLeft.classList.remove('disabled')
+        }
+        const maxPosition = -(width - slider.offsetWidth)
+        if (position <= maxPosition) {
+            btnRight.classList.add('disabled')
+        } else {
+            btnRight.classList.remove('disabled')
+        }
+    }
 
-    // console.log(row.childElementCount)
+    function move(direction) {
+        const maxPosition = -(width - row.parentElement.offsetWidth)
+        if (direction === 'right' && position > maxPosition) {
+            position -= step
+            if (position < maxPosition) {
+                position = maxPosition
+            }
+        }else  if(direction === 'left' && position < 0) {
+            position += step
+            if (position > 0) {
+                position = 0
+            }
+        }
+        row.style.transform = `translateX(${position}px)`
+        row.style.transition = `transform 0.5s ease`
+        updateBtns()
+    }
 
+    btnLeft.addEventListener('click', () => move('left'))
+    btnRight.addEventListener('click', () => move('right'))
+    window.addEventListener('resize', calc)
+    calc()
 }
