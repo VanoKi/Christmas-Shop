cf3ae40ba36251d0a801e166f9629d3867f67cd0
VanoKi <kirsvano@gmail.com>
2024-12-05 13:37:46 +0300

    feat: add a button scroll-to-top

--- christmas-shop/js/scroll-top.js
+++ christmas-shop/js/scroll-top.js
@@ -1,3 +1,25 @@
 export function scrollTop() {
+    const element = document.querySelector('.scroll-top')
+    function show() {
+      element.classList.remove('scroll-top_hide')
+    }
+    function hide() {
+      element.classList.add('scroll-top_hide')
+    }
+
+
+    window.addEventListener('scroll', () => {
+        const scrollY = window.scrollY || document.documentElement.scrollTop
+        scrollY > 300 ? show() : hide()
+    })
+
+    element.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    })
+
 
 }
