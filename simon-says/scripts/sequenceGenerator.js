366b497dd19f3dfbe48fc786971e428e309d73d3
VanoKi <kirsvano@gmail.com>
2025-01-13 20:33:05 +0300

    feat: add module greateSequence

---
+++ simon-says/scripts/sequenceGenerator.js
@@ -0,0 +1,4 @@
+const NumberSequence = Array.from({length: 10}, (v, k) => k)
+console.log(Math.floor(Math.random() * NumberSequence.length))
+console.log('a'.charCodeAt(0))
+console.log('z'.charCodeAt(0))
