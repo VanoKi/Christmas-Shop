3c32fd6538ff8b875faccf322f320bfe9f08301b
VanoKi <kirsvano@gmail.com>
2025-01-14 01:50:12 +0300

    feat: add effects to buttons

--- simon-says/scripts/keyboard.js
+++ simon-says/scripts/keyboard.js
@@ -4,6 +4,7 @@ const generateKeyboard = () => {
         'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
         'U', 'V', 'W', 'X', 'Y', 'Z'
     ];
+    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
   const keyboardContainer = document.querySelector('#keyboard');
   keyboardContainer.innerHTML = '';
   keys.forEach((key) => {
