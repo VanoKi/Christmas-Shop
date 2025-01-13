f9cc625395eb70b7bf3bf1d454ccc5650d268e97
VanoKi <kirsvano@gmail.com>
2025-01-13 22:50:06 +0300

    feat: add atribute name to selects, add event listener to btn start

---
+++ simon-says/script.js
@@ -0,0 +1,7 @@
+import {getRandomNumber} from "./scripts/sequenceGenerator.js";
+import {getRandomChar} from "./scripts/sequenceGenerator.js";
+
+document.querySelector('#start').addEventListener('click',() => {
+    // console.log('click')
+    document.querySelector('#sequence').innerText = getRandomChar()
+})
--- simon-says/scripts/sequenceGenerator.js
+++ simon-says/scripts/sequenceGenerator.js
@@ -1,11 +1,10 @@
 const NumberSequence = Array.from({length: 10}, (v, k) => k)
-// console.log(Math.floor(Math.random() * NumberSequence.length))
 const getRandomNumber = () => Math.floor(Math.random() * NumberSequence.length)
-// console.log('a'.charCodeAt(0))
-// console.log('z'.charCodeAt(0))
 const getRandomChar = () => {
     let randomNumber = Math.floor(Math.random() * (122 - 97 + 1)) + 97
     return String.fromCharCode(randomNumber)
 }
 
-console.log(getRandomChar())
+export {getRandomChar, getRandomNumber}
+
+// console.log(getRandomChar())
