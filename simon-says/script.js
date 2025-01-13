204c9dc43103f052dcc925e7461702b4a5966efb
VanoKi <kirsvano@gmail.com>
2025-01-13 22:57:42 +0300

    feat: get value of the select

--- simon-says/script.js
+++ simon-says/script.js
@@ -4,4 +4,5 @@ import {getRandomChar} from "./scripts/sequenceGenerator.js";
 document.querySelector('#start').addEventListener('click',() => {
     // console.log('click')
     document.querySelector('#sequence').innerText = getRandomChar()
+    console.log(document.querySelector('#rounds').value)
 })
