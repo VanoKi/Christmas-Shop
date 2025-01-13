e11c3ce01ad412bd88a1fc748d2960f4efdc6881
VanoKi <kirsvano@gmail.com>
2025-01-14 02:07:05 +0300

    feat: add checkInput

--- simon-says/script.js
+++ simon-says/script.js
@@ -10,4 +10,20 @@ document.querySelector('#start').addEventListener('click',() => {
     document.querySelector('#sequence').innerText = sequence
     console.log(sequence)
     generateKeyboard(level)
+
+    let userInput = [];
+    function checkInput() {
+        for (let i = 0; i < userInput.length; i++) {
+            if (userInput[i] !== sequence[i]) {
+                console.log('Error!');
+                return false;
+            }
+        }
+        if (userInput.length === sequence.length) {
+            console.log('success');
+            return true;
+        }
+        return null; // Ожидаем дальнейших нажатий
+    }
+    checkInput()
 })
