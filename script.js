c06217cce5eeee111b8df8db5ace64d7fd9e068b
VanoKi <kirsvano@gmail.com>
2025-01-28 17:34:56 +0300

    feat: add first template

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 const gridSize = 10
 const nonogram = document.getElementById('nonogram')
+
+const createBoard = () => {
+    nonogram.innerHTML = ''
+    for (let i = 0; i <gridSize * gridSize ; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('ceil');
+        cell.addEventListener('click', () => {
+            if (!cell.classList.contains('hint')) {
+                cell.classList.toggle('filled')
+            }
+        })
+        nonogram.appendChild(cell)
+    }
+}
+
+const resetGame = () => {
+  document.querySelectorAll('.cell').forEach(cell => {
+      cell.classList.remove('filled')
+  })
+}
+
+createBoard()
